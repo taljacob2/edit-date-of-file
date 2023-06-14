edcac3e92cc49086e826222b08fb75141e9819b7
Tal Jacob <taljacob2@gmail.com>
2023-06-14 23:16:51 +0300

    Add `edit-date-of-file.ps1`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ edit-date-of-file.ps1
@@ -0,0 +1,58 @@
+<#
+  .DESCRIPTION
+  A script that edits the:
+  - "last modified date"
+  - "creation date"
+  - "last access date"
+  of a file or a folder.
+
+  .PARAMETER Path
+  Specify the path to the target file or folder, that you wish to edit its dates.
+
+  .PARAMETER NewDate
+  Specify the date to be set as the new date for each:
+  - "last modified date"
+  - "creation date"
+  - "last access date"
+  By default, `NewDate` is the current date.
+
+  .INPUTS
+  None. You cannot pipe objects to this script.
+
+  .OUTPUTS
+  None. This script does not generate any output.
+
+  .NOTES
+  MIT License
+  Author: Tal Jacob
+
+  .EXAMPLE
+  PS> # Edit a file with a specific date.
+  PS> .\edit-date-of-file.ps1 -Path demo.txt -NewDate Get-Date("2020-12-31T23:54:43")
+
+  .EXAMPLE
+  PS> # Edit a folder with the current date.
+  PS> .\edit-date-of-file.ps1 -Path C:\Users\demo
+
+  .LINK
+  Online version: https://github.com/taljacob2/edit-date-of-file
+
+  .LINK
+  See `Get-Date` syntax: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7.3
+#>
+
+param (
+    [parameter(mandatory)][string]$Path,
+    [parameter(mandatory)][DateTime]$NewDate = (Get-Date))
+
+
+$file = Get-Item $Path
+
+# Edit "last modified".
+$file.LastWriteTime = $NewDate
+
+# Edit "creation date".
+$file.CreationTime = $NewDate
+
+# Edit "last access date".
+$file.LastAccessTime = $NewDate
